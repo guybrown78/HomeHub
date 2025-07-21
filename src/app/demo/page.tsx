0158da97fd5f79d0a903a8d004b66f78c9021940
Guy Brown <guy_brown@hotmail.co.uk>
2025-07-21 10:52:48 +0100

    added demo

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const DemoPage = () => {
+	return (
+		<div className="w-screen h-screen bg-black">
+			<iframe
+				src="https://embed.figma.com/proto/umD3QUDwoY6UJMF2ruGj3k/%F0%9F%8F%A0-Consult-Net-Zero---EXTERNAL?node-id=4731-1907&locale=en&embed-host=share"
+				allowFullScreen
+				className="w-full h-full border-0"
+			></iframe>
+		</div>
+	)
+}
+
+export default DemoPage
