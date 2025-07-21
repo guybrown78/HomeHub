f10b968a54b6e917dfc7e9395cc64391000f1570
Guy Brown <guy_brown@hotmail.co.uk>
2025-07-21 11:01:54 +0100

    demo starts at login

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -4,7 +4,7 @@ const DemoPage = () => {
 	return (
 		<div className="w-screen h-screen bg-black">
 			<iframe
-				src="https://embed.figma.com/proto/umD3QUDwoY6UJMF2ruGj3k/%F0%9F%8F%A0-Consult-Net-Zero---EXTERNAL?node-id=4731-1907&locale=en&embed-host=share"
+				src="https://embed.figma.com/proto/umD3QUDwoY6UJMF2ruGj3k/%F0%9F%8F%A0-Consult-Net-Zero---EXTERNAL?node-id=4731-2512&locale=en&embed-host=share"
 				allowFullScreen
 				className="w-full h-full border-0"
 			></iframe>
